7908dc3c5bf26f05828c94a8dd5f3127fc150b76
Naught33 <nevillevalentine56@gmail.com>
2023-01-24 00:03:12 +0300

    initial commit

---
+++ Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+    Private Sub btnKaribu_Click(sender As Object, e As EventArgs) Handles btnKaribu.Click
+        Dim myName As Integer
+        Dim oddoreven As String
+        Dim odd As String = "odd"
+        Dim even As String = "even"
+
+        oddoreven = TextBox1.Text
+        myName = txtName.Text
+        If oddoreven = odd Then
+            For myName = 1 To myName Step 2
+
+                MsgBox(myName)
+
+            Next
+        ElseIf oddoreven = even Then
+            For myName = 0 To myName Step 2
+                MsgBox(myName)
+            Next
+
+        End If
+
+
+
+
+    End Sub
+End Class
